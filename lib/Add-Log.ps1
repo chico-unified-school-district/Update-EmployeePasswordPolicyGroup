e18f997c3b74424656ec9a8b88746da05fd34675
jcooperCUSD <jcooper@chicousd.org>
2019-06-04 10:21:14 -0700

    lib files updated

--- lib/Add-Log.ps1
+++ lib/Add-Log.ps1
@@ -1,16 +1,16 @@
 function Add-Log {
 	[cmdletbinding()]
 	Param ( 
-		[Parameter(Position = 0, Mandatory = $True)]
+		[Parameter(Position=0,Mandatory=$True)]
 		[STRING]$Type,
-  [Parameter(Position = 1, Mandatory = $True)]
+  [Parameter(Position=1,Mandatory=$True)]
   [Alias("Msg")]
 		[STRING]$Message,
-  [Parameter(Position = 2, Mandatory = $false)]
-  [SWITCH]$Test )
+  [Parameter(Position=2,Mandatory=$false)]
+  [SWITCH]$WhatIf )
  $date = Get-Date -Format s
  $type = "[$($type.toUpper())]"
- $testString = if ($Test) { "[WhatIf] " }
- $logMsg = "$testString$date,$type,$message"
+ $WhatIfString = if ($WhatIf){"[WhatIf] "}
+ $logMsg = "$WhatIfString$date,$type,$message"
  Write-Output $logMsg
 }
